1a61a1d3193644a984ee6bf57d9e1435b6dad36f
rjwatt42 <r.j.watt42@hotmail.co.uk>
2025-11-24 20:28:17 +0000

    Update server.R

--- server.R
+++ server.R
@@ -5,17 +5,17 @@ library('ggplot2')
 
 server <- function(input, output) {
   
-  lineLength<-rlineLength<-0.2
-  lineWidth<-rlineWidth<-0.015
-  lineCol<-rlineCol<-'#000'
-  
+  # lineLength<-rlineLength<-0.2
+  # lineWidth<-rlineWidth<-0.015
+  # lineCol<-rlineCol<-'#000'
+  # 
   observeEvent({c(input$New,input$lineLength,input$lineWidth,input$lineCol,
                   input$glineOn,input$glineOffset,input$glineCol,
                   input$rlineOn,input$rlineLength,input$rlineWidth,input$rlineCol,input$rlineOffset)}, {
     lineLength<-max(0.015,input$lineLength)
-    lineWidth<-max(0.015,input$lineWidth)
+    lineWidth<-max(0.005,input$lineWidth)
     rlineLength<-max(0.015,input$rlineLength)
-    rlineWidth<-max(0.015,input$rlineWidth)
+    rlineWidth<-max(0.005,input$rlineWidth)
     g<-makeLines(useHTML=TRUE,
                  lineLength=lineLength,lineWidth=lineWidth,lineCol=input$lineCol,
                  guideLines=input$glineOn,guideOffset=input$glineOffset,guideCol=input$glineCol,
